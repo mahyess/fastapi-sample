6b9c226d98b16266214accb79b8f608d648f8d44
Mahesh Prajapati <zephyrr2722@gmail.com>
2023-06-08 12:46:26 +0545

    added unsed import and variables to test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
+    import datetime
+    hello = 1
     return {"message": "Hello World without errors"}
